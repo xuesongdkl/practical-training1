a50af1b46224f24889234798e7928c82e0966aea
xuesongdkl <13233490803@163.com>
2019-04-07 11:59:54 +0000

    debug

--- resources/views/user/center.blade.php
+++ resources/views/user/center.blade.php
@@ -16,11 +16,11 @@
                 'http://xuesong.shansister.com/center1',
                 {token:token,uid:uid},
                 function(data){
-                    alert(data)
-                    /*if(data==2){
+//                    alert(data)
+                    if(data==2){
                         alert('此账号已有其他用户登录');
                         location.href='/userlogin';
-                    }*/
+                    }
                 }
             );
         };
